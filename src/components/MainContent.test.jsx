d9293f76c094c47fb16f8761e30443398d27f9fc
Abhishek Verma <abhishekverma@Abhisheks-MacBook-Air.local>
2024-04-13 09:16:16 +0100

    event - dev

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -2,6 +2,8 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+// add comment
+
 import MainContent from './MainContent';
 
 describe('MainContent', () => {
